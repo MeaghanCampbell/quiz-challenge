eeda2a9696ba55631e135feeda955b5557bef631
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-07 20:31:13 -0600

    initial CSS & HTML

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Question Array
+
+let questions = [
+    {
+        question: "Which HTML element contains JavaScript?",
+        choiceA: "<script>",
+        choiceB: "<link>",
+        choiceC: "<meta>",
+        correct: "A"
+    },
+    {
+        question: "What are Javascript Arrays wrapped in?",
+        choiceA: "Curly Brackets {}",
+        choiceB: "Parentheses ()",
+        choiceC: "Square Brackets []",
+        correct: "C"
+    },
+    {
+        question: "Which of the following is not a data type used in JavaScript?",
+        choiceA: "Boolean",
+        choiceB: "Signal",
+        choiceC: "String",
+        correct: "B"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choiceA: "var functionName = function() { functionCode }",
+        choiceB: "var functionName = (function) [ functionCode ]",
+        choiceC: "function : functionName ( functionCode )",
+        correct: "A"
+    }
+];
