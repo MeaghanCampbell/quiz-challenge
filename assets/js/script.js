26fef309a53f247f380785d81b3ee764328d19de
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-08 22:10:26 -0600

    cycle through questions finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,11 +40,12 @@ let startQuiz = function (event) {
     // targets the start button
     var targetEl = event.target;
 
-    if (targetEl.matches(".btn")) {
+    if (targetEl.matches("#start-btn")) {
         runFirstQuestion();
     }
 }
 
+// run first question
 let runFirstQuestion = function () {
 
     // remove start button and paragraph text
@@ -62,67 +63,97 @@ let runFirstQuestion = function () {
     var choiceOneEl = document.createElement("button");
     choiceOneEl.textContent = questions[0].choiceA
     choiceOneEl.className = "btn";
-    choiceOneEl.setAttribute("id", "choice-content");
+    choiceOneEl.setAttribute("id", "first-choice");
     document.getElementById("choices").appendChild(choiceOneEl);
 
     // create choice B
     var choiceTwoEl = document.createElement("button");
     choiceTwoEl.textContent = questions[0].choiceB
     choiceTwoEl.className = "btn";
-    choiceTwoEl.setAttribute("id", "choice-content");
+    choiceTwoEl.setAttribute("id", "second-choice");
     document.getElementById("choices").appendChild(choiceTwoEl);
 
     // create choice C
     var choiceThreeEl = document.createElement("button");
     choiceThreeEl.textContent = questions[0].choiceC
     choiceThreeEl.className = "btn";
-    choiceThreeEl.setAttribute("id", "choice-content");
+    choiceThreeEl.setAttribute("id", "third-choice");
     document.getElementById("choices").appendChild(choiceThreeEl);
-    
-    nextQuestion();
+
+
+    let firstAnswerClick = document.querySelector("#first-choice")
+    let secondAnswerClick = document.querySelector("#second-choice")
+    let thirdAnswerClick = document.querySelector("#third-choice")
+    firstAnswerClick.addEventListener("click", runNextQuestion);
+    secondAnswerClick.addEventListener("click", runNextQuestion);
+    thirdAnswerClick.addEventListener("click", runNextQuestion);
+
 
 }
 
-let nextQuestion = function (event) {
-
-    // targets second button question choice
-    var targetQ = event.target;
-
-    if (targetQ.matches(".btn")) {
-        runNextQuestion();
-    }
-}
-
+// run second question
 let runNextQuestion = function () {
+
     // select question title and add text from questions array
     
     var secondQuestionTitleEl = document.querySelector("#question-title");
     secondQuestionTitleEl.textContent = questions[1].question
 
     // create choice A
-    var secondChoiceOneEl = document.querySelector("#choice-content");
+    var secondChoiceOneEl = document.querySelector("#first-choice");
     secondChoiceOneEl.textContent = questions[1].choiceA
     document.getElementById("choices").appendChild(secondChoiceOneEl);
 
     // create choice B
-    var secondChoiceTwoEl = document.querySelector("#choice-content");
+    var secondChoiceTwoEl = document.querySelector("#second-choice");
     secondChoiceTwoEl.textContent = questions[1].choiceB
     document.getElementById("choices").appendChild(secondChoiceTwoEl);
 
     // create choice C
-    var secondChoiceThreeEl = document.querySelector("#choice-content");
+    var secondChoiceThreeEl = document.querySelector("#third-choice");
     secondChoiceThreeEl.textContent = questions[1].choiceC
-    document.getElementById("choices").appendChild(secondChoiceTwoEl);
+    document.getElementById("choices").appendChild(secondChoiceThreeEl);
+
+    let firstAnswerClick = document.querySelector("#first-choice")
+    let secondAnswerClick = document.querySelector("#second-choice")
+    let thirdAnswerClick = document.querySelector("#third-choice")
+    firstAnswerClick.addEventListener("click", runLastQuestion);
+    secondAnswerClick.addEventListener("click", runLastQuestion);
+    thirdAnswerClick.addEventListener("click", runLastQuestion);
+
+
+}
+
+// run last question
+let runLastQuestion = function () {
+
+    // select question title and add text from questions array
+    
+    var thirdQuestionTitleEl = document.querySelector("#question-title");
+    thirdQuestionTitleEl.textContent = questions[2].question
+
+    // create choice A
+    var thirdChoiceOneEl = document.querySelector("#first-choice");
+    thirdChoiceOneEl.textContent = questions[2].choiceA
+    document.getElementById("choices").appendChild(thirdChoiceOneEl);
+
+    // create choice B
+    var thirdChoiceTwoEl = document.querySelector("#second-choice");
+    thirdChoiceTwoEl.textContent = questions[2].choiceB
+    document.getElementById("choices").appendChild(thirdChoiceTwoEl);
+
+    // create choice C
+    var thirdChoiceThreeEl = document.querySelector("#third-choice");
+    thirdChoiceThreeEl.textContent = questions[2].choiceC
+    document.getElementById("choices").appendChild(thirdChoiceThreeEl);
 
 }
 
 
-
-
 // let them know answer is correct with function?
+let checkAnswer = function() {
 
-// listening for second question button click to move to last
-startQuizBtnEl.addEventListener("click", nextQuestion);
+}
 
 // listening for the start button click to move to first question
 startQuizBtnEl.addEventListener("click", startQuiz);
